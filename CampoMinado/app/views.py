55895d649d9c443804224fe1e73314f972b402b5
Helio Reis <jhelioreis@hotmail.com>
2018-05-26 10:20:11 -0300

    meu commit

---
+++ CampoMinado/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .forms import JogadaForm
+def post_list(request):
+	if request.method == "POST":
+		form = JogadaForm(request.POST)
+		if form.is_valid():
+			jogada = form.save(commit=False)
+			jogada.autor = request.user
+			jogada.created_date = timezone.now()
+			jogada.save()
+	else:
+		form = JogadaForm()
+
+	return render(request, 'post_list.html', {'form': form})
+# Create your views here.
